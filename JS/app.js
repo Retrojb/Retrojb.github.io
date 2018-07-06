7c2279501f8a812182eb5865436321e2ace11852
Retrojb <baltes.26@osu.edu>
2018-07-06 15:10:04 -0400

    added drop down descriptions

--- JS/app.js
+++ JS/app.js
@@ -14,15 +14,18 @@ function carousel() {
     setTimeout(carousel, 2000); // Change image every 2 seconds
 };
 
+var coll = document.getElementsByClassName("collapse");
+var i;
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-  document.body.style.backgroundColor = "white";
-}
