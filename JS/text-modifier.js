85d353772e8e3ae2e4d025288542bf717b7ab943
Retrojb <baltes.26@osu.edu>
2018-09-23 10:10:58 -0400

    overhaul

---
+++ JS/text-modifier.js
@@ -0,0 +1,12 @@
+const hideText = document.getElementById('hideBio');
+
+console.log('poop');
+
+hideText.onclick = function(){
+
+} 
+
+
+function initElement() {
+    var p = document.getElementById("foo");
+    p.onclick = showAlert;}
